73c4be0c5aed8ae8b4b43d6088ffa449a263d509
Бобков Александр Кириллович <akbobkov@vtb.ru>
2020-11-02 11:05:33 +0300

    fixes task1

--- src/main/java/ru/geekbrains/lesson9/homework/Task1.java
+++ src/main/java/ru/geekbrains/lesson9/homework/Task1.java
@@ -1,42 +1,38 @@
 package ru.geekbrains.lesson9.homework;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.*;
+import java.nio.file.Path;
 
 public class Task1 {
 
 
-    private final String file = "C:\\Users\\admim\\IdeaProjects\\geek-vtb-java-core\\" +
-            "src\\main\\java\\ru\\geekbrains\\lesson9\\homework\\task1.txt";
-
-    public static void main(String[] args) {
-
-        Task1 task1 = new Task1();
-
-        task1.numberOfRepetitions(task1.file, "aaa");
-
-
+    public static int countSequence(Path path, String sequence) throws IOException {
+        try (BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+            StringBuilder sb = new StringBuilder();
+            int count = 0;
+            int nextChar;
+            while ((nextChar = reader.read()) != -1) {
+                if (sb.length() < sequence.length()) {
+                    sb.append((char) nextChar);
+                    continue;
+                } else if (sb.toString().equals(sequence)) {
+                    count++;
+                }
+                sb.deleteCharAt(0);
+                sb.append((char) nextChar);
+            }
+            if (sb.toString().equals(sequence)) {
+                count++;
+            }
+            return count;
+        }
     }
 
-
-    public void numberOfRepetitions(String file, String repetition) {
-        try {
-            List<String> lines = Files.readAllLines(Paths.get(file), StandardCharsets.UTF_8);
-            Set<String> words = new HashSet<>(lines);
-            int i = 0;
-            for (String word : words) {
-                if (word.contains(repetition)) {
-                    i++;
-                }
-            }
-            System.out.println("Number of repetitions: " + i);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws IOException {
+        System.out.println(countSequence(Path.of("task1.txt."), "aaa"));
     }
 
 
