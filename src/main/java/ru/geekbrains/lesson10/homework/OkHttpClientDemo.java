15303dbdcc843955687e42f1fab37400da9f4910
Бобков Александр Кириллович <akbobkov@vtb.ru>
2020-11-03 21:09:44 +0300

    homework10

---
+++ src/main/java/ru/geekbrains/lesson10/homework/OkHttpClientDemo.java
@@ -0,0 +1,24 @@
+package ru.geekbrains.lesson10.homework;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class OkHttpClientDemo {
+
+    public static void main(String[] args) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://ya.ru")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        System.out.println(response.code());
+        System.out.println(response.body().string());
+
+    }
+
+}
